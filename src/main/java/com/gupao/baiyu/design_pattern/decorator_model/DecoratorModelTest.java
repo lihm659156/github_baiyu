194c1e74e44c44ae46b81cbffcc84c7628920793
lihm659156 <865622375@qq.com>
2018-03-17 19:45:22 +0800

    修饰者设计模式

--- src/main/java/com/gupao/baiyu/design_pattern/decorator_model/DecoratorModelTest.java
+++ src/main/java/com/gupao/baiyu/design_pattern/decorator_model/DecoratorModelTest.java
@@ -1,9 +1,9 @@
 package main.java.com.gupao.baiyu.design_pattern.decorator_model;
 
-public class App {
+public class DecoratorModelTest {
 
     public static void main(String[] args) {
-        System.out.println("111");
+
     }
 
 }
