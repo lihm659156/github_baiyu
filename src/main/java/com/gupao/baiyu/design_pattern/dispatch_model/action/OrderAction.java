6c9f0e4fe81a2b7dcb99d022ec6145dd232b4ea5
lihm659156 <865622375@qq.com>
2018-03-21 17:36:20 +0800

    委派模式

---
+++ src/main/java/com/gupao/baiyu/design_pattern/dispatch_model/action/OrderAction.java
@@ -0,0 +1,9 @@
+package com.gupao.baiyu.design_pattern.dispatch_model.action;
+
+public class OrderAction {
+
+    public void getOrderById(String id){
+        System.out.println("查询订单");
+    }
+
+}
---
+++ src/main/java/com/gupao/baiyu/design_pattern/dispatch_model/action/UserAction.java
@@ -0,0 +1,9 @@
+package com.gupao.baiyu.design_pattern.dispatch_model.action;
+
+public class UserAction {
+
+    public void getUserById(String id){
+        System.out.println("查询用户");
+    }
+
+}
