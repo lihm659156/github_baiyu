fee8e3677ccbef2f8dc4d5802f828681b5341820
lihm659156 <865622375@qq.com>
2018-03-24 22:34:54 +0800

    mybatis

--- src/main/java/com/gupao/baiyu/mybatis/Test.java
+++ src/main/java/com/gupao/baiyu/mybatis/Test.java
@@ -1,4 +1,10 @@
 package com.gupao.baiyu.mybatis;
 
 public class Test {
+
+    // 1.什么是Mybatis?
+    // 2.去官网学习
+    // 3.java 枚举类型使用，mybatis中的TypeHandler处理
+    // 4.mybatis 的plugins 是什么？有啥用？
+
 }
