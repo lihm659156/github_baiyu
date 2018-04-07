eb1e21c374387da4927195a23cfcf2e58f361139
lihm659156 <865622375@qq.com>
2018-04-08 07:46:17 +0800

    java spi

--- src/main/java/com/gupao/baiyu/dubbo/spi/Test.java
+++ src/main/java/com/gupao/baiyu/dubbo/spi/Test.java
@@ -12,6 +12,10 @@ public class Test {
         for(ISayName sayName : loaders){
             sayName.say();
         }
+
+        // java spi 缺点：
+        // 1.java标准的spi会一次性实例化扩展点所有实现，如果没用用上，则浪费资源
+        // 2.如果扩展点加载失败，连扩展点的名称都拿不到。
     }
 
 }
