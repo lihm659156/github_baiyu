4e99e9ccc59ce3e25703e352a8cf05f2bfe0c5ea
lihm659156 <865622375@qq.com>
2018-04-10 15:11:36 +0800

    AIO

---
+++ src/main/java/com/gupao/baiyu/nio/aio/AIOClient.java
@@ -0,0 +1,70 @@
+package com.gupao.baiyu.nio.aio;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+
+public class AIOClient {
+
+    private final AsynchronousSocketChannel client;
+
+    public AIOClient() throws Exception{
+        client = AsynchronousSocketChannel.open();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new AIOClient().connect("localhost",8000);
+    }
+
+    public void connect(String host, int port) throws Exception{
+        client.connect(new InetSocketAddress(host, port), null, new CompletionHandler<Void, Void>() {
+
+            @Override
+            public void completed(Void result, Void attachment) {
+                try{
+                    client.write(ByteBuffer.wrap("这是一条测试数据".getBytes())).get();
+                    System.out.println("已发送至服务器");
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+
+            @Override
+            public void failed(Throwable exc, Void attachment) {
+                exc.printStackTrace();
+            }
+        });
+
+        // allocate JVM内存分配,allocateDirect 系统内存分配
+        final ByteBuffer bb = ByteBuffer.allocate(1024);
+
+        client.read(bb, null, new CompletionHandler<Integer, Object>() {
+            @Override
+            public void completed(Integer result, Object attachment) {
+                System.out.println("IO操作完成"+result);
+                System.out.println("获取反馈结果"+new String(bb.array()));
+            }
+
+            @Override
+            public void failed(Throwable exc, Object attachment) {
+                exc.printStackTrace();
+            }
+        });
+
+        Thread.sleep(Integer.MAX_VALUE);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
